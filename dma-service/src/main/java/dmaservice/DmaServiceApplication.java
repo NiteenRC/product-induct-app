2b228f1b0a26553758d74f01bcb16e2b292d6b0b
Satish Chougule <satishrajc@gmail.com>
2024-05-09 12:59:18 +0530

    Initial commit: service-registry, api-gateway and dma-service

---
+++ dma-service/src/main/java/dmaservice/DmaServiceApplication.java
@@ -0,0 +1,15 @@
+package dmaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class DmaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmaServiceApplication.class, args);
+	}
+
+}
