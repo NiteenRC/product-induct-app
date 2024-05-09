7fe83c3355f0f25ec618de9cd0544b09042eb9be
Satish Chougule <satishrajc@gmail.com>
2024-05-09 13:01:21 +0530

    format code

--- dma-service/src/main/java/dmaservice/DmaServiceApplication.java
+++ dma-service/src/main/java/dmaservice/DmaServiceApplication.java
@@ -8,8 +8,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class DmaServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DmaServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DmaServiceApplication.class, args);
+    }
 
 }
--- dma-service/src/test/java/dmaservice/DmaServiceApplicationTests.java
+++ dma-service/src/test/java/dmaservice/DmaServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DmaServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
