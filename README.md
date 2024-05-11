5064b0a9bba358dbeacc5177c098e3a365b5cae1
Satish Chougule <satishrajc@gmail.com>
2024-05-11 09:18:47 +0530

    circuit breaker

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # product-induct-app
+
+## Steps to follow for running micro services:
+1. Service Registry
+2. Config Server
+3. API Gateway
+4. DMA Service
+5. Product Service
--- dma-service/src/main/java/dmaservice/BrandController.java
+++ dma-service/src/main/java/dmaservice/BrandController.java
@@ -1,5 +1,6 @@
 package dmaservice;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/dma")
+@Log4j2
 public class BrandController {
 
     @GetMapping
     public ResponseEntity<?> findAllBrands() {
+        log.info("Test Instances..");
         return ResponseEntity.ok("Test Routing..");
     }
 }
